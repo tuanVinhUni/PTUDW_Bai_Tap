7e8dcd79d5e34f70e6b373c721596d41dfa66182
tuanVinhUni <225748020110138@vinhuni.edu.vn>
2024-12-13 00:20:08 +0700

    Add ContactsController and ProductNew view component with layout updates

---
+++ TH_Harmic/ViewComponents/ProductNewViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TH_Harmic.Models;
+
+namespace TH_Harmic.ViewComponents
+{
+  
+    public class ProductNewViewComponent:ViewComponent
+    {
+        private readonly TH_HarmicContext _context;
+        public  ProductNewViewComponent(TH_HarmicContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbProducts.Include(m => m.CategoryProduct).Where(m => (bool)m.IsActive).Where(m => m.IsNew);
+            return await Task.FromResult<IViewComponentResult>(View(items.OrderByDescending(m => m.ProductId).ToList()));
+        }
+    }
+    
+}
